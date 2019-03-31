b89d1fe9cd6ac4b364d5249f106f1cc6ed964bb1
ctkolb <chris@phase7.com>
2019-03-30 22:57:19 -0500

    added int contains

--- contains.go
+++ contains.go
@@ -4,3 +4,12 @@ func StringMapContains(set map[string]int, item string) bool {
 	_, ok := set[item]
 	return ok
 }
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
